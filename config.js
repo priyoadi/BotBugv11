1ba0598edb83a7f761c2cdd672d49d4bcc5d0761
priyoadi <118030324+priyoadi@users.noreply.github.com>
2022-12-03 19:26:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -22,7 +22,7 @@ global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['6288214551869','60177426853','60177426853']
+global.owner = ['6288214551869','17146136919','60177426853']
 global.premium = ['60177426853']
 global.packname = 'The Jo Bot'
 global.author = 'WhatsApp Bot'
